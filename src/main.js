67909f334ffeeef20b72ee42873e1f40a5f66379
WhiteLeaf03 <2876202234@qq.com>
2023-04-12 17:31:38 +0800

    添加animate.css

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import './style.css'
 import App from './App.vue'
 import {router} from "./expand/router.js";
 import {store} from "./expand/store.js";
+import animated  from 'animate.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(animated)
 app.mount('#app')
