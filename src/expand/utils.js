0287b504e0d73302cc561b398720163304999e9a
WhiteLeaf03 <2876202234@qq.com>
2023-04-14 11:21:15 +0800

    使用ChatGPT修复优化时间转换工具

--- src/expand/utils.js
+++ src/expand/utils.js
@@ -16,12 +16,14 @@ export function getCookie(name) {
 
 //时间转换
 export function tsToDate(timestamp) {
-    let date = new Date(parseInt(timestamp)).toLocaleString().replace(/:\d{1,2}$/,' ');
-    if (date !== null) {
-        return date
-    } else {
-        return 'error'
-    }
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hour = ('0' + date.getHours()).slice(-2);
+    const minute = ('0' + date.getMinutes()).slice(-2);
+    const second = ('0' + date.getSeconds()).slice(-2);
+    return year + '-' + month + '-' + day + ' ' + hour + ':' + minute + ':' + second;
 }
 
 export function dateToTimestamp(date) {
