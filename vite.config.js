f0439f8451f54ff3e6182f8ad6b7f6895c77631f
WhiteLeaf03 <2876202234@qq.com>
2023-04-15 18:24:09 +0800

    设置反向代理

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 80
+    port: 80,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true
+      }
+    }
   }
 })
